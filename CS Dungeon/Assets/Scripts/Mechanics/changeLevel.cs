dc599829a5f1a14e418523f498628df66bde01b3
CUTmayne <andrewtucr@gmail.com>
2020-05-06 19:54:51 -0400

    level1-2
    a few changes and level transition

---
+++ CS Dungeon/Assets/Scripts/Mechanics/changeLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+using UnityEngine.SceneManagement;
+
+namespace Platformer.Mechanics
+{
+
+    public class changeLevel : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            var p = collider.gameObject.GetComponent<PlayerController>();
+            if (p != null)
+            {
+                
+                p.SendMessage("Finished");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ CS Dungeon/Assets/Scripts/Mechanics/vertical_platform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+public class vertical_platform : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float distance = 10.0f;
+    public float initialPoint;
+    public float endPoint;
+    public bool endReached = false;
+    
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPoint = transform.position.y;
+        endPoint = initialPoint + 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = speed * Time.deltaTime;
+
+        //transform.Translate(sineTranslate, 0, 0);
+
+        //If the x position of the platform is around the value of its starting position,
+        //then endReached becomes false. If it reaches the endPoint x value, then endReached 
+        //becomes true
+        if (transform.position.y <= initialPoint)
+        {
+            endReached = false;
+        }
+        else if (transform.position.y >= endPoint)
+        {
+            endReached = true;
+        }
+        //if endReached is false then the platform moves to the right. If true then to the left
+        if (endReached == false)
+        {
+            transform.Translate(0, translation, 0);
+        }
+        else
+        {
+            transform.Translate(0, -translation, 0);
+        }
+    }
+}

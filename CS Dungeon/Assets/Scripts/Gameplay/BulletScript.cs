c41f3f72256eaa9e34f328c5f75d6eb4c4f6973f
wlee412 <60402577+wlee412@users.noreply.github.com>
2020-05-06 22:11:59 -0400

    Add files via upload

---
+++ CS Dungeon/Assets/Scripts/Gameplay/BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour{
+
+    public float velX = 5f;
+    public float velY = 0f;
+    Rigidbody2D rb;
+    public float distance;
+    public LayerMask isLayer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Invoke("DestroyBullet",1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, transform.right, distance, isLayer);
+        if(ray.collider != null)
+        {
+            DestroyBullet();
+        }
+        rb.velocity = new Vector2(velX,velY);   
+    }
+   
+
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+
+}
---
+++ CS Dungeon/Assets/Scripts/Gameplay/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{   
+    
+    public int tokenCount = 0;
+    private AudioSource audio;
+    public AudioClip pickUpItem;
+
+    private void Start()
+    {
+        this.audio = this.gameObject.AddComponent<AudioSource>();
+        this.audio.clip = this.pickUpItem;
+        this.audio.loop = false;
+    }
+    public void PickupItem(GameObject obj)
+    {
+        switch (obj.tag)
+        {
+            case "Token":
+                tokenCount++;
+                break;
+            
+            default:
+                this.audio.Play();
+                break;
+            
+        }
+
+        
+    }
+}

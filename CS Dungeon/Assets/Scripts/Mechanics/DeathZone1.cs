499f000ccedd6522c60d5c1511b1fedfcf85e7f9
CUTmayne <andrewtucr@gmail.com>
2020-05-06 22:43:02 -0400

    small additions
    death zones

---
+++ CS Dungeon/Assets/Scripts/Mechanics/DeathZone1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+using UnityEngine.SceneManagement;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// DeathZone components mark a collider which will schedule a
+    /// PlayerEnteredDeathZone event when the player enters the trigger.
+    /// </summary>
+    public class DeathZone1 : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            var p = collider.gameObject.GetComponent<PlayerController>();
+            if (p != null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
